56d3a075d858e351ad64395a2ecd6cfd2b7cacdd
조아빈 <whdkqls122@naver.com>
2025-04-02 17:37:24 +0900

    [1주차] 조아빈 - 수 스택, 스택수열

---
+++ 아빈/1주차/수/스택수열.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+		int [] arr = new int[N];
+		int length = 0;
+		int num = 1;
+		for(int i = 0; i < N; i++){
+			int input = Integer.parseInt(br.readLine());
+			if(num <= input){
+				while(num <= input) {
+					arr[length] = num++;
+					length++;
+					sb.append("+\n");
+				}
+				arr[length - 1] = 0;
+				length--;
+				sb.append("-\n");
+			} else {
+				if (arr[length - 1] > input) {
+					System.out.println("NO");
+					System.exit(0);
+				}
+				arr[length - 1] = 0;
+				length--;
+				sb.append("-\n");
+			}
+		}
+		System.out.println(sb);
+	}
+}
