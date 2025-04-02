ae7a39454c1e45381869a64d92342923b454b273
조아빈 <whdkqls122@naver.com>
2025-04-02 19:31:09 +0900

    refactor : 클래스 이름 변경 Main -> 스택수열

--- 아빈/1주차/수/스택수열.java
+++ 아빈/1주차/수/스택수열.java
@@ -1,6 +1,6 @@
 import java.io.*;
 
-public class Main {
+public class 스택수열 {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
