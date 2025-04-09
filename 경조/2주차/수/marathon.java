c8c06b815d511f2278aaaa9c61c016f6b6632a34
Gyeongjo <119joe@naver.com>
2025-04-09 20:19:17 +0900

    [2주차] 정경조 - 수 세로 읽기

---
+++ 경조/2주차/수/marathon.java
@@ -0,0 +1,24 @@
+class marathon {
+    public String marathon(String[] participant, String[] completion) {
+        String answer = "";
+        boolean[] complete = new boolean[participant.length];
+
+        for (int i = 0; i < completion.length; i++) {
+            for (int j = 0; j < participant.length; j++) {
+                if (!complete[j] && participant[j].equals(completion[i])) {
+                    complete[j] = true;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < participant.length; i++) {
+            if (!complete[i]) {
+                answer = participant[i];
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
