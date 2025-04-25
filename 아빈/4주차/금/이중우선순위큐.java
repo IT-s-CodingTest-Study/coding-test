c5843c20edf58aacc2a91752ed4226d136bf430d
조아빈 <whdkqls122@naver.com>
2025-04-25 21:51:54 +0900

    [4주차] 조아빈 - 금 이중우선순위 큐

---
+++ 아빈/4주차/금/이중우선순위큐.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+	public int[] solution(String[] operations) {
+		Map<Integer, Integer> tree = new TreeMap<>();
+
+		for (String op : operations) {
+			String[] str = op.split(" ");
+			int num = Integer.parseInt(str[1]);
+
+			if ("I".equals(str[0])) {
+				tree.put(num, tree.getOrDefault(num, 0) + 1);
+			} else {
+				if (tree.isEmpty()) continue;
+
+				int key = (num == 1) ? ((TreeMap<Integer, Integer>) tree).lastKey()
+					: ((TreeMap<Integer, Integer>) tree).firstKey();
+
+				if (tree.get(key) == 1) {
+					tree.remove(key);
+				} else {
+					tree.put(key, tree.get(key) - 1);
+				}
+			}
+		}
+
+		if (tree.isEmpty()) return new int[]{0, 0};
+
+		return new int[]{((TreeMap<Integer, Integer>) tree).lastKey(),
+			((TreeMap<Integer, Integer>) tree).firstKey()};
+	}
+}
