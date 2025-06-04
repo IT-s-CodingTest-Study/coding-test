11214b8294a9e70634858343ba07594f344ffe5f
jjang0308 <jang653838@naver.com>
2025-06-04 23:03:23 +0900

    [10주차] 장혁 - 수 정수 삼각형

---
+++ 혁/10주차/수/정수_삼각형.java
@@ -0,0 +1,16 @@
+package 수;
+
+public class 정수_삼각형 {
+    public int solution(int[][] triangle) {
+        int n = triangle.length;
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                triangle[i][j] += Math.max(triangle[i + 1][j], triangle[i + 1][j + 1]);
+            }
+        }
+
+        return triangle[0][0];
+    }
+}
+
