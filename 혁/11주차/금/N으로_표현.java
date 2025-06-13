b575ac0d4170b7f2540ff50dbbae103ba93acd92
jjang0308 <jang653838@naver.com>
2025-06-13 22:55:21 +0900

    [11주차] 장혁 - 금 N으로 표현

---
+++ 혁/11주차/금/N으로_표현.java
@@ -0,0 +1,41 @@
+package 금;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class N으로_표현 {
+
+    public int solution(int N, int number) {
+        if (N == number) return 1;
+
+        List<Set<Integer>> dp = new ArrayList<>();
+
+        for (int i = 0; i <= 8; i++) {
+            dp.add(new HashSet<>());
+        }
+
+        for (int i = 1; i <= 8; i++) {
+            int repeated = Integer.parseInt(String.valueOf(N).repeat(i));
+            dp.get(i).add(repeated);
+
+            for (int j = 1; j < i; j++) {
+                for (int x : dp.get(j)) {
+                    for (int y : dp.get(i - j)) {
+                        dp.get(i).add(x + y);
+                        dp.get(i).add(x - y);
+                        dp.get(i).add(x * y);
+                        if (y != 0) dp.get(i).add(x / y);
+                    }
+                }
+            }
+
+            if (dp.get(i).contains(number)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
